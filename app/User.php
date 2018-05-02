6af4230202a609e9ff78eb88e55da2c6f843de4f
nixxx27 <nikko.zabala@gmail.com>
2018-05-02 11:54:29 +0800

    initial

---
+++ app/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','role'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+
+    //check if admin . = 1
+    public function is_admin()
+    {
+        if($this->role == 'admin')
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+}
